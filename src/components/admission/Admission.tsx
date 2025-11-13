98d410fc531293bdafa0f2311a78888a8c25c719
Arathi-k-v <arathi@signroots.com>
2025-11-13 17:14:32 +0530

    Update Admission.tsx

--- src/components/admission/Admission.tsx
+++ src/components/admission/Admission.tsx
@@ -47,10 +47,9 @@ const Admission = () => {
 
           {/* Contact Details */}
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold mb-4">For more details</h2>
+            <h2 ></h2>
             <p>
-              <strong>Dept.of Al-Manar:</strong> <a href="tel:+919895413809" className="text-blue-600 underline">9895413809</a><br />
-              <strong>Others:</strong> <a href="tel:+919567871424" className="text-blue-600 underline">9567871424</a>, <a href="tel:+917593078368" className="text-blue-600 underline">7593078368</a>
+              <strong>For more details , please contact</strong> <a href="tel:+919567871424" className="text-blue-600 underline">9567871424</a>, <a href="tel:+917593078368" className="text-blue-600 underline">7593078368</a>
             </p>
           </div>
           {/* Admission Form */}
